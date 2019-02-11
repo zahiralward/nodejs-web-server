c6bd527a70f8a54179d0c60a39a50320c0ddc687
Zahir Alward <zwfalward@hotmail.com>
2019-02-11 22:53:05 +1300

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const hbs = require("hbs");
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Hello this is my page"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    message: "Bad Address"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("The server has started on port 3000");
+});
